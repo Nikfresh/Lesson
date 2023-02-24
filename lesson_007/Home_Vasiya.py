ebccac575f7f48429f1e93c5f75ac8a10539c640
Glozman Nikita <best.svetovik@mail.com>
2023-02-24 18:58:58 +0300

    бивис и бадхед вьезжают в дом не законченно

--- lesson_007/Home_Vasiya.py
+++ lesson_007/Home_Vasiya.py
@@ -3,6 +3,18 @@ from random import randint
 from termcolor import cprint
 
 
+class House:
+    """Дом"""
+
+    def __init__(self):
+        self.food = 10
+        self.money = 100000
+        self.name = 'Домищще'
+
+    def __str__(self):
+        return f"Дом - {self.name}, еды осталось - {self.food}, денег есть - {self.money}"
+
+
 class Man:
     """ Человек"""
 
@@ -11,9 +23,11 @@ class Man:
         self.fullness = 50
         self.food = 40
         self.money = 30
+        self.house = House
+        self.house.name = "бездомный"
 
     def __str__(self):
-        return f"Я - {self.name}, сытость - {self.fullness}, еды осталось - {self.food}, денег есть - {self.money}"
+        return f"Я - {self.name}, сытость - {self.fullness}, еды осталось - {self.food}, денег есть - {self.money}, живет в  - {self.house.name}"
 
     def eat(self):
         if self.food >= 10:
@@ -40,7 +54,7 @@ class Man:
     def act(self):
         dise = randint(1, 6)
         if self.fullness < 5:
-            print(f"{self.name} умер от переистощения" )
+            print(f"{self.name} умер от переистощения")
             return
         if self.money <= 20:
             self.work()
@@ -48,7 +62,6 @@ class Man:
             self.shop_food()
         elif self.fullness <= 20:
             self.eat()
-
         elif dise == 1:
             self.work()
         elif dise == 2:
@@ -56,12 +69,33 @@ class Man:
         else:
             self.play_dota()
 
+    def go_in_house(self, home):
+        if self.house.name == "бездомный":
+            self.house = home
+            print(f"{self.name} заехал в {self.house.name}")
+            self.fullness -= 10
+            self.house.money += self.money
+            self.money = home.money
+        else: cprint(f"{self.name} уже живет в {self.house.name} ему не надо заселяться снова",color='red')
 
-vasya = Man('Вася')
 
-for day in range(1, 366):
+bivis = Man('Бивис')
+budhead = Man('Бадхед')
+home_for_man = House()
+# print(bivis)
+# bivis.go_in_house(home=home_for_man)
+# print(bivis)
+# print(home_for_man)
+day_of_bivis_go_house = 3 #randint(3,10)
+day_of_budhead_go_house = 4#randint(3,10)
+for day in range(1, 10):
     print(f"=================День {day}=================", )
-    vasya.act()
-    print(vasya)
-
-
+    bivis.act()
+    budhead.act()
+    print(bivis)
+    print(budhead)
+    print(home_for_man)
+    if day == day_of_bivis_go_house:
+        bivis.go_in_house(home=home_for_man)
+    if day == day_of_budhead_go_house:
+        budhead.go_in_house(home=home_for_man)
