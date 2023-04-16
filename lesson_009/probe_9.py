4dfcef69a2ae60d378256b76d86084d059f052e9
Glozman Nikita <best.svetovik@mail.com>
2023-04-16 18:49:11 +0300

    9урок 1

--- lesson_009/probe_9.py
+++ lesson_009/probe_9.py
@@ -2,10 +2,28 @@
 import os
 import zipfile
 
-dirpath = 'D:\phython_prog_lesson\lesson_009\python_snippets'
-zip_file_name = os.path.join(dirpath, 'voyna-i-mir.txt.zip')
-zfile = zipfile.ZipFile(zip_file_name, 'r')
-for filename in zfile.namelist():
-    print(filename)
-    zfile.extract(filename,path=dirpath)
-zfile.printdir()
+# dirpath = 'python_snippets'
+# zip_file_name = os.path.join(dirpath, 'voyna-i-mir.txt.zip')
+# zfile = zipfile.ZipFile(zip_file_name, 'r')
+# for filename in zfile.namelist():
+#     print(filename)
+#     zfile.extract(filename,path=dirpath)
+# zfile.printdir()
+
+dirpath = 'python_snippets'
+filename = os.path.join(dirpath, 'voyna-i-mir.txt')
+prev_char = ' '
+stat = {}
+# stat = {'а':{'т':500,'x':300},'т':{'о':100,'у':50}}
+
+with open(filename, 'r', encoding='cp1251') as file:
+    for line in file:
+        print(line, end='')
+        for char in line:
+            if prev_char in stat:
+                if char in stat[prev_char]:
+                    stat[prev_char][char] += 1
+                else:
+                    stat[prev_char][char] = 1
+            else:
+                stat[prev_char] = {char: 1}
